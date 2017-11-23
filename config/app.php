d12153a61e5ef48b6d7586958b7402e308ee379b
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2017-11-23 14:40:46 +0200

    Added "ide-helper", "debugbar"

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * IDE Helpers...
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
